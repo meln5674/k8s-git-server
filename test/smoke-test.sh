d20da6e0cb7ab2c9b83c799df5f805374183a152
Andrew Melnick <meln5674@kettering.edu>
2023-03-19 15:18:25 -0600

    Add capability to populate repos from volumes

---
+++ test/smoke-test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -xeu
+
+CHART=$1
+VERSION=${2:-}
+
+FLAGS=( k8s-git-server "${CHART}" --values test/values.yaml )
+if [ -n "${VERSION}" ]; then
+    FLAGS+=( --version="${VERSION}" )
+fi
+
+for x in $(seq 10); do
+  helm repo update --debug
+  if helm template "${FLAGS[@]}" ; then
+      break
+  fi
+  echo 'Waiting and retrying'
+  sleep 60
+done
+
+FLAGS+=( --wait --debug --install )
+
+kubectl apply -f test/configmap.yaml
+
+kubectl delete job k8s-git-server-push || true
+( while ! kubectl logs -f job/k8s-git-server-push ; do sleep 2; done ) &
+
+helm upgrade "${FLAGS[@]}"
+
+wait || true
+
+helm test k8s-git-server --debug &
+kubectl logs -f k8s-git-server-test-connection
+
+wait
