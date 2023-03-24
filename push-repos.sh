5be7c9fc20c059528473dcd8ee57c2290e2faf62
Andrew Melnick <meln5674@kettering.edu>
2023-03-23 18:54:58 -0600

    Add option to modify how repos are populated using tar flags

--- push-repos.sh
+++ push-repos.sh
@@ -9,9 +9,7 @@ set -o pipefail
 
 # Vars:
 # EXTERNAL_HOST : Hostname of the server to push to
-# REPO_DIR : Directory that repo source volumes are mounted
-#   Each directory should be in the format of $REPO_DIR/$USER/$REPO/.k8s-git-server
-#   This trailing directory is required so that the full repo path can be determined
+# REPO_FLAGS : Extra flags when copying the repository. Copy is performed via tar c | tar x, flags are provided to tar c
 # PRIVATE_KEY_KEY : Key name to obtain user's private key in the generated secret
 # PUBLIC_KEY_KEY : Key name to obtain user's public key in the generated secret
 # KNOWN_HOSTS_KEY : Key name to obtain known_hosts in the generated secret
@@ -31,16 +29,21 @@ ls "${REPO_DIR}" | while read -r git_user ; do
     find "${user_git_dir}/" -type d -name ".k8s-git-server" | while read -r full_repo_path ; do
         repo_path="${full_repo_path##${user_git_dir}}"
         repo_path="${repo_path%%/.k8s-git-server}"
-        tmprepo=$(mktemp -d)
-        rmdir "${tmprepo}"
-        cp -r "${full_repo_path}" "${tmprepo}"
-        rm -rf "${tmprepo}/.git"
+        repo_flags_file="${user_git_dir}/${repo_path}/.flags"
+        if [ -f "${repo_flags_file}" ]; then
+            mapfile -t repo_flags < "${repo_flags_file}" 
+        else
+            repo_flags=()
+        fi
+        tmprepo="$(mktemp -d)"
+        tar -c --exclude='.git/*' -C "${full_repo_path}" "${repo_flags[@]}" . | tar -x -v -C "${tmprepo}" --no-same-owner 
         (
             cd "${tmprepo}"
             git init
+            ls -la
             git config user.name "k8s-git-server ${git_user}"
             git config user.email "${git_user}@k8s-git-server.example.com"
-            git config core.sshCommand "ssh -vvv -i '${user_private_key}' -o 'UserKnownHostsFile=${user_known_hosts}'"
+            git config core.sshCommand "ssh -v -i '${user_private_key}' -o 'UserKnownHostsFile=${user_known_hosts}'"
             git add -A
             git commit -m 'k8s-git-server'
             git remote add k8s-git-server "ssh://${git_user}@${EXTERNAL_HOST}${repo_path}"
@@ -48,3 +51,4 @@ ls "${REPO_DIR}" | while read -r git_user ; do
         )
     done
 done
+
